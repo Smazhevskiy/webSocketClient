9816feebd17384691c94c13407624b95943cf23a
user_nitro_5 <timmy.lepel@gmail.com>
2021-11-20 22:54:48 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './app.css'
+import {WebSock} from './WebSock'
+
+
+
+
+function App() {
+    return (
+        <div>
+            <WebSock/>
+        </div>
+    )
+}
+
+
+export default App
